e9ce4c5f8765f0d821f57cf2e3e5475748a74f30
truth <truthbk@gmail.com>
2012-11-28 02:24:45 +0100

    Getting unit test-suite ready - compiles, links succesfully. Early skeleton, simple implementation.

--- src/tests/test_hash.c
+++ src/tests/test_hash.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h> 
 #include "CUnit/Basic.h"
 
-/* Pointer to the file used by the tests. */
-static FILE* temp_file = NULL;
+#include "milu.h"
+#include "hashtbl/hashtbl.h"
+#include "list/list.h"
+
+/* dirty hack to get some addresses  */
+#define ADDRESS_HERE() ({ void *p; __asm__("1: mov 1b, %0" : "=r" (p)); p; })
+
 
 /* The suite initialization function.
  *  * Opens the temporary file used by the tests.
@@ -11,6 +17,7 @@ static FILE* temp_file = NULL;
  *    */
 int init_suite1(void)
 {
+    return 0;
 }
 
 /* The suite cleanup function.
@@ -19,20 +26,46 @@ int init_suite1(void)
  *    */
 int clean_suite1(void)
 {
+    uint32_t i = 0;
+    struct memalloc * mem = NULL;
+    struct hash_entry * entry = NULL;
+    struct list_head  * lh = NULL;
+
+
+    //clean this mess up ;)
+    hash_table_for_each_safe( entry, _milu_htable, lh, i ) {
+        mem = hash_entry( entry, struct memalloc, hentry );
+        hash_table_del_hash_entry( _milu_htable, entry );
+        free(mem);
+    }
+
+    return 0;
 }
 
 void testHASHCREATE(void)
 {
+    hash_table_init(_milu_htable, 100, NULL);
 }
 
 void testHASHINSERT(void)
 {
-    unsigned char buffer[20];
+    void * ptr = NULL;
+    struct memalloc * mem = NULL;
 
-    if (NULL != temp_file) {
-        rewind(temp_file);
-        CU_ASSERT(9 == fread(buffer, sizeof(unsigned char), 20, temp_file));
-        CU_ASSERT(0 == strncmp(buffer, "Q\ni1 = 10", 9));
+    if (!(mem = (struct memalloc *)malloc(sizeof(struct memalloc)))) {
+        hash_entry_init(&mem->hentry, ptr, sizeof(ptr));
+        hash_table_insert_safe( _milu_htable, &mem->hentry, ptr, sizeof(ptr) );
+
+        mem->size = 0; 
+        mem->bt_size = 0;
+
+        ptr = mem;
+        mem->ptr = ptr; //points to itself.
+        mem->calladdr = (uintptr_t) ADDRESS_HERE(); //this is just a mock address, cuz we don't have a caller
+
+
+        //CU_ASSERT(9 == fread(buffer, sizeof(unsigned char), 20, temp_file));
+        //CU_ASSERT(0 == strncmp(buffer, "Q\ni1 = 10", 9));
     }
 }
 
@@ -50,6 +83,19 @@ void testHASHCOLLIDE(void)
 
 void testHASHDESTROY(void)
 {
+    uint32_t i = 0;
+    struct memalloc * mem = NULL;
+    struct hash_entry * entry = NULL;
+    struct list_head  * lh = NULL;
+
+
+    //clean this mess up ;)
+    hash_table_for_each_safe( entry, _milu_htable, lh, i ) {
+        mem = hash_entry( entry, struct memalloc, hentry );
+        hash_table_del_hash_entry( _milu_htable, entry );
+        free(mem->bt);
+        free(mem);
+    }
 }
 
 /* The main() function for setting up and running the tests.
